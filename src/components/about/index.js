19c8c8166685f81c31e1acd320ed7dce7fb9f6c3
Clfreeman22 <clfreeman22@gmail.com>
2022-01-09 07:20:28 -0600

    data on page

---
+++ src/components/about/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div class="myinfo">
+                <h2>Charles "Charlie" Freeman</h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga id quae alias sint quia ipsam quod. Quis, minima repudiandae molestiae laudantium voluptates iusto asperiores, cum accusamus deserunt corrupti nobis sequi.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem totam perferendis quam dolorem, explicabo rerum sed architecto sunt at quasi quo incidunt vel porro eaque cum numquam adipisci ut. Harum.
+                </p>
+            </div>
+    )
+}
+
+export default About;
