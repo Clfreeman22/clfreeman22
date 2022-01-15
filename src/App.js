2d09d6a2792dcf2dfa0518688004dec3f22f80f2
Clfreeman22 <clfreeman22@gmail.com>
2022-01-15 07:40:31 -0600

    nav bar works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import About from './components/about';
 import Nav from './components/nav';
+import Contact from './components/contact'
 
 function App() {
   return (
     <div>
-      <Nav />
+      <Nav></Nav>
       <main>
-        <About />
+        <About></About>
       </main>
     </div>
   );
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 const Nav = () => {
+    const categories = [
+        { name: 'products', description: "Products that I have worked on.",},
+        { name: 'coding', description: "Languages and libraries I enjoy the most." },
+        { name: 'resume', description: "My Resume" },
+      ];
+      
+      function categorySelected(name) {
+        console.log(`${name} clicked`)
+    }
+
     return(
         <header>
            <h1>
                <a herf="/">Charles Lee Freeman III</a>
            </h1>
            <nav>
-               <ul>
-                   <li>
+               <ul className='flex-row'>
+                   <li className='mx-2'>
                        <a href="#about-me">About Me</a>
                    </li>
                    <li>
-                       <a href="#my-projects">Projects</a>
+                       <span>Contact Me</span>
                    </li>
-                   <li>
-                       <a href="#contact-me">Contact Me</a>
-                   </li>
-                   <li>
-                      <a href="./resume.html">Resume</a> 
-                   </li>
+                   {categories.map((category) => (
+                       <li className='mx-1' key={category.name}>
+                           <span onClick={() => {categorySelected(category.name); }} >
+                                {capitalizeFirstLetter(category.name)}
+                            </span>
+                       </li>
+                   ))}
                </ul>
            </nav>  
         </header>
