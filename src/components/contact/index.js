b8a6734501dc9c22b4669ba751e3e5c373565a93
Clfreeman22 <clfreeman22@gmail.com>
2022-04-19 13:37:57 -0500

    added email validation to contact page

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -5,29 +5,54 @@ const Contact = () => {
 
   const [formState, setFormState] = useState({ name: '', email: '', message: ''});
   const {name, email, message} = formState
+  const [errorMessage, setErrorMessage] = useState('');
 
   function handleChange(e) {
+    
+    if (e.target.name === 'email') { 
 
+      const isValid = validateEmail(e.target.value);
+
+        if(!isValid) {
+          setErrorMessage('Please enter a valid email.');
+        }
+        else {
+          if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+          }
+          else {
+            setErrorMessage('');
+          }
+        }
+    }
     setFormState({...formState, [e.target.name]: e.target.value})
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
     <section>
       <h1>Contact Me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor='name'>Name:</label>
-          <input type='text' name='name' defaultValue={name} onChange={handleChange}/>
+          <input type='text' name='name' defaultValue={name} onBlur={handleChange}/>
         </div>
         <div>
           <label htmlFor='email'>Email Address:</label>
-          <input type='email' name='email' defaultValue={email} onChange={handleChange}/>
+          <input type='email' name='email' defaultValue={email} onBlur={handleChange}/>
         </div>
         <div>
           <label htmlFor='message'>Message:</label>
-          <textarea name='message' rows='5' defaultValue={message} onChange={handleChange}/>
+          <textarea name='message' rows='5' defaultValue={message} onBlur={handleChange}/>
         </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
         <button type='submit'>Submit</button>
       </form>
 
@@ -36,3 +61,4 @@ const Contact = () => {
 }
 
 export default Contact;
+
